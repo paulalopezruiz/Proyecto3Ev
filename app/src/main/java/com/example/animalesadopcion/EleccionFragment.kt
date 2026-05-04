1b155530241efc72ed9e5845368f5ccb959185d3
paulalopezruiz <paula.lopez51397@ikasle.egibide.org>
2026-05-04 21:52:01 +0200

    Mas fragmentos y inicio de logica de inicio de sesion / registro

---
+++ app/src/main/java/com/example/animalesadopcion/EleccionFragment.kt
@@ -0,0 +1,6 @@
+package com.example.animalesadopcion
+
+import androidx.fragment.app.Fragment
+
+class EleccionFragment : Fragment(R.layout.fragment_eleccion) {
+}
--- app/src/main/java/com/example/animalesadopcion/SecondFragment.kt
+++ app/src/main/java/com/example/animalesadopcion/SecondFragment.kt
@@ -1,44 +1,25 @@
 package com.example.animalesadopcion
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.animalesadopcion.databinding.FragmentSecondBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment() {
-
-    private var _binding: FragmentSecondBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
-        return binding.root
-
-    }
+class SecondFragment : Fragment(R.layout.fragment_second) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
-    }
+        val btnIniciarSesion = view.findViewById<Button>(R.id.btnIniciarSesion)
+        val btnRegistrarse = view.findViewById<Button>(R.id.btnRegistrarse)
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        btnIniciarSesion.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_LoginFragment)
+        }
+
+        btnRegistrarse.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_RegisterFragment)
+        }
     }
 }
